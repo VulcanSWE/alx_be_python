ff910f0200cab8a960c3acdc3c346784ade6db68
chef-vulcan <lilcrystalhimself@gmail.com>
2025-07-19 03:08:27 +0100

    added a directory python_introduction with a python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 20
+number2 = 10
+
+additon = number1 + number2
+
+subtraction = number1 - number2
+
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {additon}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
