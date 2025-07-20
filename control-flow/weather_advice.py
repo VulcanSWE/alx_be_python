24129d34a3676bafcde73249fa76d4336421c707
chef-vulcan <lilcrystalhimself@gmail.com>
2025-07-20 18:31:31 +0100

    modified the file weather_advice.py again

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 #Prompt the user for the weather condition and provide advice based on the input
-weather = str(input("Enter the weather condition (sunny, rainy, snowy): ")).lower()
+weather = input("Enter the weather condition (sunny, rainy, snowy): ").lower()
 
 # Check the weather condition and provide appropriate advice
 if weather == "sunny":
-    print(" Wear a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print(" Don't forget your umbrella and a raincoat..")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "snowy":
-    print("Make sure to wear a warm coat and a scarf..")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
